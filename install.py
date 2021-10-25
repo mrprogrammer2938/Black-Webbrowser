fdb4f5cda3b3f6827a60180b9c747c31825e9127
Mr.programmer2938 <78996423+mrprogrammer2938@users.noreply.github.com>
2021-10-25 15:52:29 +0330

    Update install.py

--- install.py
+++ install.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
-# black-Webbrowser v1.5
-import os,subprocess,platform
+# Black-Webbrowser v1.5
+import os,subprocess
 try:
     from tkinter import *
     from tkinter.ttk import *
-    from tkinter.messagebox import showerror
     from ttkbootstrap import Style
 except ImportError:
     subprocess.getoutput("pip install tk-tools")
@@ -12,33 +11,34 @@ except ImportError:
 class black_webbrowser(Tk):
     def __init__(self):
         super(black_webbrowser,self).__init__()
-        self.title('Hacker-Webbrowser/Uninstalling')
+        self.title('black-Webbrowser/Installing')
+        self.photo = PhotoImage(file = './Scr/black.png')
         self.style = Style("cyborg")
-        label_l = Label(self,text='Hacker-Webbrowser',background='white',foreground='black',font=("None",15))
+        label_l = Label(self,text='black-Webbrowser',background='white',foreground='black',font=("None",15))
         label_l.place(bordermode=INSIDE,x=120,y=20)
-        self.uninstall_b = Button(self,text='uninstall',command=self.uninstall)
-        self.uninstall_b.place(bordermode=OUTSIDE,x=160,y=110)
-        self.exit_b = Button(self,text='Exit',command=self.ext)
-        self.exit_b.place(bordermode=OUTSIDE,x=160,y=135)
-        self.photo = PhotoImage(file = './Scr/black/png')
-        self.iconphoto(False,self.photo)
+        self.install_b = Button(self,text='Install',width=9,command=self.install)
+        self.install_b.place(bordermode=OUTSIDE,x=160,y=110)
+        self.exit_b = Button(self,text='Exit',width=9,command=self.ext)
+        self.exit_b.place(bordermode=OUTSIDE,x=160,y=145)
+
         self.config(background='white')
+        self.iconphoto(False,self.photo)
         self.resizable(False,False)
         self.geometry("400x300")
         self.mainloop()
-    def uninstall(self):
+    def install(self):
         global pr
         pr = Progressbar(self,orient='horizontal',mode='determinate',length=200)
         pr.place(bordermode=INSIDE,x=105,y=65)
         pr.start(55)
-        pr.after(6000,self.uninstall_2)
-    def uninstall_2(self):
-        subprocess.getoutput("cd .. && rm -r black-Webbrowser")
+        pr.after(6000,self.install_2)
+    def install_2(self):
+        subprocess.getoutput("chmod a+x black.py")
         pr.stop()
         pr.destroy()
         label_mess = Label(self,text='Complete!',foreground='black',background='white')
         label_mess.place(bordermode=INSIDE,x=170,y=65)
-        self.uninstall_b.destroy()
+        self.install_b.destroy()
         self.exit_b.place(bordermode=OUTSIDE,x=160,y=110)
 
     def ext(self):
@@ -46,10 +46,4 @@ class black_webbrowser(Tk):
         self.quit()
         quit()
 if __name__ == '__main__':
-    if platform.system() == 'Linux':
-        if os.getuid() == 0:
-          window = black_webbrowser()
-        else:
-            showerror(title='Cannot Running',message='Please, Check Root!')
-    else:
-        window = black_webbrowser()
+    window = black_webbrowser()
