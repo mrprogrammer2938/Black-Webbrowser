9b36cbf59ad51e033811104d79cc0415621bb802
Sina Meysami <78996423+mrprogrammer2938@users.noreply.github.com>
2023-07-22 16:05:16 +0330

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,17 @@
+# Black-Webbrowser Installing
+#
+
+echo "Installing..."
+echo ""
+
+sudo apt install python
+sudo apt install python3
+
+pip install PyQt5
+pip install -r requirements.txt
+
+
+echo "Finish...!"
+echo "Use: python black.py"
+
+exit
